034f2803ae60003614b29bf1038855d88a0fa4c1
Schmidt Robert <robstoner19@gmail.com>
2023-06-20 17:45:20 +0300

    More stuff done

---
+++ proiect/R/ex_10.r
@@ -0,0 +1,121 @@
+source("functie_exemplu.r")
+
+ex_1 <- function(set_valori) {
+  # Calculați mediana, media și deviația standard
+  mediana <- median(set_valori)
+  media <- mean(set_valori)
+  deviatia_standard <- sd(set_valori)
+
+  # Afișați valorile calculate
+  cat("Mediana:", mediana, "\n")
+  cat("Media:", media, "\n")
+  cat("Deviația standard:", deviatia_standard, "\n")
+
+  # Afișați histograma
+  hist(set_valori, main = "Histograma", xlab = "Valori", ylab = "Frecventa", col = "lightblue", breaks = "FD")
+
+  # Adăugați linii verticale pentru mediana, media și deviația standard
+  abline(v = mediana, col = "red", lwd = 2)
+  abline(v = media, col = "blue", lwd = 2)
+  abline(v = c(media - deviatia_standard, media + deviatia_standard), col = "green", lwd = 2, lty = 2)
+}
+
+ex_2 <- function(set) {
+  # Ajustarea distribuției normale
+  fit_norm <- MASS::fitdistr(set[set > 0], "normal")
+  AIC_norm <- AIC(fit_norm)
+  BIC_norm <- BIC(fit_norm)
+
+  # Ajustarea distribuției Poisson
+  fit_pois <- MASS::fitdistr(set[set > 0], "Poisson")
+  AIC_pois <- AIC(fit_pois)
+  BIC_pois <- BIC(fit_pois)
+
+  # Ajustarea distribuției exponențiale
+  fit_exp <- MASS::fitdistr(set[set > 0], "exponential")
+  AIC_exp <- AIC(fit_exp)
+  BIC_exp <- BIC(fit_exp)
+
+  # Ajustarea distribuției gamma
+  fit_gamma <- MASS::fitdistr(set[set > 0], "gamma")
+  AIC_gamma <- AIC(fit_gamma)
+  BIC_gamma <- BIC(fit_gamma)
+
+  # Compararea criteriilor AIC și BIC
+  criteria <- data.frame(
+    Distributie = c("Normala", "Poisson", "Exponentiala", "Gamma"),
+    AIC = c(AIC_norm, AIC_pois, AIC_exp, AIC_gamma),
+    BIC = c(BIC_norm, BIC_pois, BIC_exp, BIC_gamma)
+  )
+  criteria
+
+  # Alegerea distribuției cu cele mai mici criterii AIC și BIC
+  distributie_AIC <- criteria$Distributie[which.min(criteria$AIC)]
+  distributie_BIC <- criteria$Distributie[which.min(criteria$BIC)]
+
+  print(paste("Distributia potrivita conform criteriului AIC este:", distributie_AIC))
+  print(paste("Distributia potrivita conform criteriului BIC este:", distributie_BIC))
+}
+
+ex_3 <- function(set) {
+  mle <- MASS::fitdistr(set, "normal")$estimate
+
+  moment <- c(mean(set), sd(set))
+
+  comparatie <- data.frame(
+    Parametri = c("Media", "Deviatia standard"),
+    MLE = mle,
+    Moment = moment
+  )
+  comparatie
+}
+
+ex_4 <- function(set) {
+  test <- shapiro.test(set)
+
+  rezultate <- data.frame(
+    P_Value = test$p.value,
+    Normala = NA
+  )
+
+  rezultate$Normala[rezultate$P_Value > 0.05] <- "Da"
+  rezultate$Normala[rezultate$P_Value <= 0.05] <- "Nu"
+
+  rezultate
+}
+
+main <- function() {
+  # Citirea setului de valori
+  set_valori <- readline(prompt = "Introduceti setul de valori (separate prin virgula): ")
+  set_valori <- as.numeric(strsplit(set_valori, ",")[[1]])
+
+  # Meniu interactiv
+  while (TRUE) {
+    cat("\nAlegeți opțiunea dorită:")
+    cat("\n1. Histograma, mediana, media și deviația standard")
+    cat("\n2. Identificarea repartiției")
+    cat("\n3. Estimarea parametrilor în baza celor 5 eșantioane")
+    cat("\n4. Verificarea verosimilității extragerii dintr-o repartiție normală")
+    cat("\n0. Ieșire\n")
+
+    optiune <- readline(prompt = "Opțiune: ")
+
+    if (optiune == "0") {
+      break
+    }
+
+    if (optiune == "1") {
+      ex_1(set_valori)
+    } else if (optiune == "2") {
+      ex_2(set_valori)
+    } else if (optiune == "3") {
+      ex_3(set_valori)
+    } else if (optiune == "4") {
+      ex_4(set_valori)
+    } else {
+      cat("Opțiune invalidă! Vă rugăm să alegeți o opțiune validă.\n")
+    }
+  }
+}
+
+main()
